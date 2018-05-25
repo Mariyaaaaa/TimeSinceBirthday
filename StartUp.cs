09860e459c413deda5adccbaef49d25eaeda4958
Mariya <mariqvalkova@gmail.com>
2018-05-25 12:38:51 +0300

    TimeSinceBirthday
    Converting years into days,into hours, into minutes

---
+++ StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+   public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+            var years = int.Parse(Console.ReadLine());
+
+            int days = years * 365;
+            int hours = days * 24;
+            int minutes = hours * 60;
+
+            Console.WriteLine($"{years} years= {days} days= {hours} hours= {minutes} minutes.");
+        } 
+           
+    }
+
